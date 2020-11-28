73a5e10f21aaff85d1d35f130afeed550fe8eb45
miss-deadapple <miss-deadapple@gmail.com>
2020-11-28 20:35:58 +0300

    15-16 lab push.

---
+++ 15_16_lab/15_16_lab/Calculator.cpp
@@ -0,0 +1,15 @@
+#include "Calculator.h"
+
+using namespace std;
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ arg) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	My1516lab::Calculator form; //имя вашего проекта
+	Application::Run(% form);
+
+};
---
+++ 15_16_lab/15_16_lab/Calculator.h
@@ -0,0 +1,282 @@
+#pragma once
+
+namespace My1516lab {
+
+
+	float a, b, c, d, S;
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для Calculator
+	/// </summary>
+	public ref class Calculator : public System::Windows::Forms::Form
+	{
+	public:
+		Calculator(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~Calculator()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Calculator::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(18, 19);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(483, 23);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Расчет площали по формуле: S = 78*(a/b+c/d)";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(41, 87);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(43, 23);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"a =";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(41, 127);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(43, 23);
+			this->label3->TabIndex = 1;
+			this->label3->Text = L"b =";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(41, 167);
+			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(43, 23);
+			this->label4->TabIndex = 1;
+			this->label4->Text = L"c =";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(41, 207);
+			this->label5->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(43, 23);
+			this->label5->TabIndex = 1;
+			this->label5->Text = L"d =";
+			// 
+			// textBox1
+			// 
+			this->textBox1->BackColor = System::Drawing::Color::Black;
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox1->ForeColor = System::Drawing::Color::Aqua;
+			this->textBox1->Location = System::Drawing::Point(94, 83);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(137, 31);
+			this->textBox1->TabIndex = 2;
+			this->textBox1->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &Calculator::textBox1_KeyPress);
+			// 
+			// textBox2
+			// 
+			this->textBox2->BackColor = System::Drawing::Color::Black;
+			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox2->ForeColor = System::Drawing::Color::Aqua;
+			this->textBox2->Location = System::Drawing::Point(94, 123);
+			this->textBox2->Margin = System::Windows::Forms::Padding(4);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(137, 31);
+			this->textBox2->TabIndex = 2;
+			this->textBox2->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &Calculator::textBox2_KeyPress);
+			// 
+			// textBox3
+			// 
+			this->textBox3->BackColor = System::Drawing::Color::Black;
+			this->textBox3->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox3->ForeColor = System::Drawing::Color::Aqua;
+			this->textBox3->Location = System::Drawing::Point(94, 163);
+			this->textBox3->Margin = System::Windows::Forms::Padding(4);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(137, 31);
+			this->textBox3->TabIndex = 2;
+			this->textBox3->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &Calculator::textBox3_KeyPress);
+			// 
+			// textBox4
+			// 
+			this->textBox4->BackColor = System::Drawing::Color::Black;
+			this->textBox4->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox4->ForeColor = System::Drawing::Color::Aqua;
+			this->textBox4->Location = System::Drawing::Point(94, 203);
+			this->textBox4->Margin = System::Windows::Forms::Padding(4);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(137, 31);
+			this->textBox4->TabIndex = 2;
+			this->textBox4->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &Calculator::textBox4_KeyPress);
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Purple;
+			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->ForeColor = System::Drawing::Color::White;
+			this->button1->Location = System::Drawing::Point(297, 203);
+			this->button1->Margin = System::Windows::Forms::Padding(4);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(160, 33);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Вычислить";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &Calculator::button1_Click);
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(308, 150);
+			this->label6->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(43, 23);
+			this->label6->TabIndex = 4;
+			this->label6->Text = L"S =";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(348, 150);
+			this->label7->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(0, 23);
+			this->label7->TabIndex = 5;
+			// 
+			// Calculator
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(11, 23);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Black;
+			this->ClientSize = System::Drawing::Size(516, 268);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Font = (gcnew System::Drawing::Font(L"Consolas", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->ForeColor = System::Drawing::Color::Aqua;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Margin = System::Windows::Forms::Padding(4);
+			this->Name = L"Calculator";
+			this->Text = L"Calculator";
+			this->Load += gcnew System::EventHandler(this, &Calculator::Calculator_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		try {
+			a = System::Double::Parse(this->textBox1->Text);
+			b = System::Double::Parse(this->textBox2->Text);
+			c = System::Double::Parse(this->textBox3->Text);
+			d = System::Double::Parse(this->textBox4->Text);
+
+			S = 78 * (a / b + c / d);
+
+			label7->Text = S.ToString();
+		} catch (Exception ^e)	{
+			label7->Text = "Ошибка ввода.";
+		}
+
+	}
+
+private: System::Void textBox1_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+	if ((e->KeyChar >= '0') && (e->KeyChar <= '9') || e->KeyChar == 127 || e->KeyChar == 8 || e->KeyChar == ',') return;
+	e->Handled = true;
+}
+private: System::Void textBox2_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+	if ((e->KeyChar >= '0') && (e->KeyChar <= '9') || e->KeyChar == 127 || e->KeyChar == 8 || e->KeyChar == ',') return;
+	e->Handled = true;
+}
+private: System::Void textBox3_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+	if ((e->KeyChar >= '0') && (e->KeyChar <= '9') || e->KeyChar == 127 || e->KeyChar == 8 || e->KeyChar == ',') return;
+	e->Handled = true;
+}
+private: System::Void textBox4_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+	if ((e->KeyChar >= '0') && (e->KeyChar <= '9') || e->KeyChar == 127 || e->KeyChar == 8 || e->KeyChar == ',') return;
+	e->Handled = true;
+}
+private: System::Void Calculator_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+
+};
+}
